69a1addb6090b133ab73e718a8d2daec3efb2468
Buch <25643076+buchita@users.noreply.github.com>
2020-04-11 10:19:44 +0100

    Update admin.py

--- projectApp/admin.py
+++ projectApp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Flower
+from .models import Flower#, TrainModel
 
 # Register your models here.
 admin.site.register(Flower)
-# admin.site.register(ImageModel)
+
+
+# admin.site.register(TrainModel)
